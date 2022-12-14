5e340f472c8022bfdb7f2a84b325448de755d062
JRToyoda <therubikators@gmail.com>
2022-12-14 16:03:43 +0800

    Version 2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 data = [5, 3, 7]
 
+# 1. Assume that the variable data refers to the list [5, 3, 7]. Write the values of the following expressions:
+
 a = data[2]  # 7
 b = data[-1]  # 7
 c = len(data)  # 3
@@ -7,6 +9,8 @@ d = data[0:2]  # [5, 3]
 e = 0 in data  # False
 f = data + [2, 10, 5]  # [5, 3, 7, 2, 10, 5]
 g = tuple(data)  # (5, 3, 7)
+
+# 2. Assume that the variable data refers to the list [5, 3, 7]. Write the expressions that perform the following tasks:
 
 data[0] = -data[0]
 data.append(10)
