8c759f49c2cd8d8378ec9b9b0369791e172b6b95
JRToyoda <therubikators@gmail.com>
2022-12-15 16:47:53 +0800

    Version 2

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ data = [5, 3, 7]
 
 # 1. Assume that the variable data refers to the list [5, 3, 7]. Write the values of the following expressions:
 
-a = data[2]  # 7
-b = data[-1]  # 7
-c = len(data)  # 3
-d = data[0:2]  # [5, 3]
-e = 0 in data  # False
-f = data + [2, 10, 5]  # [5, 3, 7, 2, 10, 5]
-g = tuple(data)  # (5, 3, 7)
+print(data[2])  # 7
+print(data[-1])  # 7
+print(len(data))  # 3
+print(data[0:2])  # [5, 3]
+print(0 in data)  # False
+print(data + [2, 10, 5])  # [5, 3, 7, 2, 10, 5]
+print(tuple(data))  # (5, 3, 7)
 
 # 2. Assume that the variable data refers to the list [5, 3, 7]. Write the expressions that perform the following tasks:
 
@@ -18,6 +18,4 @@ data.insert(2, 22)
 del data[1]
 data.sort()
 
-# [-5, 7, 10, 22]
-
-print()  # input any variable from above
+print(data)  # [-5, 7, 10, 22]
